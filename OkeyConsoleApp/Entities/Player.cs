ab6e87771518084102be709f37d028a8ec6c8c6b
Onur Eymen <onurceymen@yahoo.com>
2025-01-28 10:33:13 +0300

    Entityler Eklendi

---
+++ OkeyConsoleApp/Entities/Player.cs
@@ -0,0 +1,35 @@
+﻿using OkeyConsoleApp.Model;
+
+namespace OkeyConsoleApp.Entities
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public List<Tile> Hand { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Hand = new List<Tile>();
+        }
+
+        public void DrawTile(Tile tile)
+        {
+            Hand.Add(tile);
+        }
+
+        public void DropTile(Tile tile)
+        {
+            Hand.Remove(tile);
+        }
+
+        public void DisplayHand()
+        {
+            Console.WriteLine($"{Name}'in Taşları:");
+            for (int i = 0; i < Hand.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}: {Hand[i]}");
+            }
+        }
+    }
+}
---
+++ OkeyConsoleApp/Entities/Tile.cs
@@ -0,0 +1,20 @@
+﻿namespace OkeyConsoleApp.Model
+{
+    public class Tile
+    {
+        public string Color { get; set; }
+        public int Number { get; set; }
+        public bool IsFakeJoker { get; set; }
+
+        public Tile(string color, int number, bool ısFakeJoker = false)
+        {
+            Color = color;
+            Number = number;
+            IsFakeJoker = ısFakeJoker;
+        }
+        public override string ToString()
+        {
+            return IsFakeJoker ? "Sahte Okey" : $"{Color} {Number}";
+        }
+    }
+}
