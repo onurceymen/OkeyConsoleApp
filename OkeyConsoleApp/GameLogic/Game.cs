867b5db858fd20dbfa60d2b89f9384fb2800eb0c
Onur Eymen <onurceymen@yahoo.com>
2025-01-28 11:22:36 +0300

    İlk denemesel yapılar oluşturuldu

---
+++ OkeyConsoleApp/GameLogic/Game.cs
@@ -0,0 +1,83 @@
+﻿using OkeyConsoleApp.Entities;
+using OkeyConsoleApp.Model;
+
+namespace OkeyConsoleApp.GameLogic
+{
+    public class Game
+    {
+        private List<Tile> TileBag { get; set; }
+        private List<Player> Players { get; set; }
+        private int CurrentPlayerIndex { get; set; }
+
+        public Game(List<Player> players)
+        {
+            Players = players;
+            TileBag = CreateTiles();
+            ShuffleTiles();
+            DistributeTiles();
+            CurrentPlayerIndex = 0;
+        }
+
+        private List<Tile> CreateTiles()
+        {
+            var colors = new[] { "Kırmızı", "Mavi", "Sarı", "Siyah" };
+            var tiles = new List<Tile>();
+
+            foreach (var color in colors)
+            {
+                for (int i = 0; i < 13; i++)
+                {
+                    tiles.Add(new Tile(color, i));
+                    tiles.Add(new Tile(color, i));
+
+                }
+            }
+
+            tiles.Add(new Tile("Sahte", 0, true));
+            tiles.Add(new Tile("Sahte", 0, true));
+
+            return tiles;
+        }
+
+        private void ShuffleTiles()
+        {
+            Random rnd = new Random();
+            TileBag = TileBag.OrderBy(x => rnd.Next()).ToList();
+        }
+
+        private void DistributeTiles()
+        {
+            for (int i = 0; i < Players.Count; i++)
+            {
+                for (int j = 0; j < 14; j++)
+                {
+                    Players[i].DrawTile(TileBag.First());
+                    TileBag.RemoveAt(0);
+                }
+            }
+        }
+
+        public void StartGame()
+        {
+            while (true)
+            {
+                var currentPlayer = Players[CurrentPlayerIndex];
+                Console.Clear();
+                Console.WriteLine($"Sıra: {currentPlayer.Name}");
+                currentPlayer.DisplayHand();
+
+                Console.WriteLine("Bir Taş Seçip Atın");
+                int selectedTileIndex = int.Parse(Console.ReadLine());
+
+                var selectedTile = currentPlayer.Hand[selectedTileIndex];
+                currentPlayer.DropTile(selectedTile);
+                Console.WriteLine($"{selectedTile} taşını attınız!");
+
+                CurrentPlayerIndex = (CurrentPlayerIndex + 1) % Players.Count;
+
+                Console.WriteLine("Devam etmek için bir tuşa basın...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
--- OkeyConsoleApp/Program.cs
+++ OkeyConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OkeyConsoleApp.Entities;
+using OkeyConsoleApp.GameLogic;
+
+var players = new List<Player>
+        {
+            new Player("Oyuncu 1"),
+            new Player("Oyuncu 2"),
+            new Player("Oyuncu 3"),
+            new Player("Oyuncu 4")
+        };
+
+var game = new Game(players);
+game.StartGame();
